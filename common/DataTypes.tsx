36ff8bd0afdafdea47241069d870ad70429c9969
uidude <22799484+uidude@users.noreply.github.com>
2023-04-04 07:26:36 -0700

    First pass at data model

--- common/DataTypes.tsx
+++ common/DataTypes.tsx
@@ -8,19 +8,29 @@
  */
 
 import {User} from '@toolkit/core/api/User';
-import {BaseModel, DeletedBy, Field, Model, Ref} from '@toolkit/data/DataStore';
+import {
+  BaseModel,
+  DeletedBy,
+  Field,
+  Model,
+  Ref,
+  TString,
+} from '@toolkit/data/DataStore';
 
 // These are the fields that will be copied from the user type
 // to the profile type that is visible to other users
 export const PROFILE_FIELDS = ['pic', 'name'];
 
+export type ThingType = 'book';
+export type ExternalType = 'openLibrary';
 @Model({name: 'things'})
-@DeletedBy(Ref('creator'))
 export class Thing extends BaseModel {
   @Field() name: string;
   @Field() description: string;
+  @Field(TString) type: ThingType;
   @Field() imageUrl: string;
-  @Field() creator: User;
+  @Field(TString) externalId: string;
+  @Field(TString) externalType: ExternalType;
 }
 
 @Model({name: 'faves'})
@@ -28,4 +38,19 @@ export class Thing extends BaseModel {
 export class Fave extends BaseModel {
   @Field() user: User;
   @Field() thing: Thing;
+}
+
+export type TagType = 'library' | 'ignored';
+
+/**
+ * Per-user private tags
+ * library = user has read or is reading (and future will be watched / is watching
+ * ignored = user saw this in a list and doesn't want to be asked about it again
+ */
+@Model({name: 'tags'})
+@DeletedBy(Ref('user'), Ref('thing'))
+export class Tags extends BaseModel {
+  @Field() user: User;
+  @Field() thing: Thing;
+  @Field(TString) type: TagType;
 }
