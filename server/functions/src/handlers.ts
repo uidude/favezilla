d79cb76716fbde7299448020c59b81bdc5c9c155
uidude <22799484+uidude@users.noreply.github.com>
2023-04-27 23:25:41 -0700

    Use new profile logic for server user functions

--- server/functions/src/handlers.ts
+++ server/functions/src/handlers.ts
@@ -1,9 +1,18 @@
-import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
-import {AuthData} from 'firebase-functions/lib/common/providers/https';
-import {Profile, User, UserRoles} from '@toolkit/core/api/User';
+import {
+  ADD_FAVE,
+  ADD_THING,
+  BROADCAST_ADMIN_NOTIF,
+  GET_USER,
+  SEND_ADMIN_NOTIF,
+  SEND_FAVE_NOTIF,
+  SEND_THING_DELETE_NOTIF,
+  UPDATE_USER,
+} from '@app/common/Api';
+import {Fave, Profile, Thing} from '@app/common/DataTypes';
+import {NOTIF_CHANNELS} from '@app/common/NotifChannels';
+import {User, UserRoles} from '@toolkit/core/api/User';
 import {CodedError} from '@toolkit/core/util/CodedError';
-import {Updater} from '@toolkit/data/DataStore';
+import {Updater, getRequired} from '@toolkit/data/DataStore';
 import {firebaseStore} from '@toolkit/providers/firebase/DataStore';
 import {
   requireAccountInfo,
@@ -16,23 +25,30 @@ import {
   getDataStore,
 } from '@toolkit/providers/firebase/server/Firestore';
 import {registerHandler} from '@toolkit/providers/firebase/server/Handler';
-import {apnsToFCMToken} from '@toolkit/providers/firebase/server/PushNotifications';
-import {getSender} from '@toolkit/providers/firebase/server/PushNotifications';
-import {getAllowlistMatchedRoles} from '@toolkit/providers/firebase/server/Roles';
 import {
-  ADD_FAVE,
-  ADD_THING,
-  BROADCAST_ADMIN_NOTIF,
-  GET_USER,
-  SEND_ADMIN_NOTIF,
-  SEND_FAVE_NOTIF,
-  SEND_THING_DELETE_NOTIF,
-  UPDATE_USER,
-} from '@app/common/Api';
-import {Fave, PROFILE_FIELDS, Thing} from '@app/common/DataTypes';
-import {NOTIF_CHANNELS} from '@app/common/NotifChannels';
+  apnsToFCMToken,
+  getSender,
+} from '@toolkit/providers/firebase/server/PushNotifications';
+import {getAllowlistMatchedRoles} from '@toolkit/providers/firebase/server/Roles';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import {AuthData} from 'firebase-functions/lib/common/providers/https';
 
 const firebaseConfig = getFirebaseConfig();
+
+function newProfileFor(user: Updater<User>): Updater<Profile> {
+  const id = user.id!;
+  return {
+    id,
+    user: {id},
+    name: user.name,
+    ...(user.pic && {pic: user.pic}),
+  };
+}
+
+function addDerivedFields(user: User) {
+  user.canLogin = true;
+}
 
 /**
  * Convert Firebase Auth account to User
@@ -41,70 +57,63 @@ async function accountToUser(auth: AuthData): Promise<User> {
   // TODO: Make `firestore` role-based (e.g. firestoreForRole('ACCOUNT_CREATOR'))
   // @ts-ignore
   const users = await getDataStore(User);
-  const user = await users.get(auth.uid, {edges: [UserRoles]});
-  if (user != null) {
+  const profiles = await getDataStore(Profile);
+  const userId = auth.uid;
+
+  let [user, profile] = await Promise.all([
+    users.get(userId, {edges: [UserRoles]}),
+    profiles.get(userId),
+  ]);
+  if (user != null && profile != null) {
+    addDerivedFields(user);
     return user;
   }
 
+  // TODO: Potentially fix this logic
   // If the user matches any role, make them a user.
   const roles = await getAllowlistMatchedRoles(auth);
   if (roles.length === 0) {
     throw new CodedError('AUTH.ERROR', 'You are not in allowlist');
   }
 
+  const firebaseAccount = auth.token!;
+
+  const name =
+    firebaseAccount.displayName ||
+    firebaseAccount.email ||
+    firebaseAccount.phoneNumber ||
+    'No Name';
+
   const newUser: User = {
     id: auth.uid,
-    name:
-      auth.token?.name ||
-      auth.token?.email ||
-      auth.token?.phone_number ||
-      'No Name',
-    canLogin: true,
+    name,
+    pic: firebaseAccount.picture || undefined,
+    email: firebaseAccount.email || undefined,
   };
 
-  if (auth.token?.picture != null) {
-    newUser.pic = auth.token.picture;
+  const newProfile = newProfileFor(newUser);
+
+  // We have an example of doing these in a transaction (in server code)
+  // but for simplicity, will make separate calls.
+  if (user == null) {
+    user = await users.create(newUser);
+    addDerivedFields(user);
   }
 
-  if (auth.token?.email != null) {
-    newUser.email = auth.token.email;
+  if (profile == null) {
+    await profiles.create(newProfile);
   }
 
-  const newProfile: Profile = {
-    id: newUser.id,
-    user: newUser,
-    name: newUser.name,
-  };
-  PROFILE_FIELDS.forEach(pField => {
-    if (pField in newUser) {
-      // @ts-ignore
-      newProfile[pField] = newUser[pField];
-    }
-  });
+  //return user;
 
-  const adminFirestore = admin.firestore();
-  await adminFirestore.runTransaction(async (transaction: any) => {
-    const userStoreInTxn = firebaseStore(
-      User,
-      // @ts-ignore: hack to pass in `transaction`
-      adminFirestore,
-      transaction,
-      firebaseConfig,
-    );
-    const profileStoreInTxn = firebaseStore(
-      Profile,
-      // @ts-ignore: hack to pass in `transaction`
-      adminFirestore,
-      transaction,
-      firebaseConfig,
-    );
-    const rolesStoreInTxn = firebaseStore(
-      UserRoles,
-      // @ts-ignore: hack to pass in `transaction`
-      adminFirestore,
-      transaction,
-      firebaseConfig,
-    );
+  const fs = admin.firestore();
+  await fs.runTransaction(async (txn: any) => {
+    // @ts-ignore: hack to pass in `transaction`
+    const userStoreInTxn = firebaseStore(User, fs, txn, firebaseConfig);
+    // @ts-ignore: ditto
+    const profileStoreInTxn = firebaseStore(Profile, fs, txn, firebaseConfig);
+    // @ts-ignore: ditto
+    const rolesStoreInTxn = firebaseStore(UserRoles, fs, txn, firebaseConfig);
 
     userStoreInTxn.create({...newUser, roles: {id: newUser.id}});
     profileStoreInTxn.create(newProfile);
@@ -112,6 +121,7 @@ async function accountToUser(auth: AuthData): Promise<User> {
   });
 
   const createdUser = await users.get(newUser.id, {edges: [UserRoles]});
+  addDerivedFields(createdUser!);
   return createdUser!;
 }
 setAccountToUserCallback(accountToUser);
@@ -182,7 +192,9 @@ export const sendThingDeleteNotif = registerHandler(
 export const getUser = registerHandler(GET_USER, async () => {
   const account = requireAccountInfo();
   const store = await getDataStore(User);
-  return store.get(account.uid, {edges: [UserRoles]});
+  const user = await getRequired(store, account.uid, {edges: [UserRoles]});
+  addDerivedFields(user);
+  return user;
 });
 
 export const updateUser = registerHandler(
@@ -194,34 +206,20 @@ export const updateUser = registerHandler(
       // TODO: coded typed error
       throw new Error('Not allowed');
     }
-    const firestore = admin.firestore();
-    await firestore.runTransaction(async transaction => {
-      const userStoreInTxn = firebaseStore(
-        User,
-        // @ts-ignore: hack to pass in `transaction`
-        firestore,
-        transaction,
-        firebaseConfig,
-      );
-      const profileStoreInTxn = firebaseStore(
-        Profile,
-        // @ts-ignore: hack to pass in `transaction`
-        firestore,
-        transaction,
-        firebaseConfig,
-      );
-      const profileValues: Updater<Profile> = {id: user.id, user: user};
-      PROFILE_FIELDS.forEach(pField => {
-        if (pField in values) {
-          // @ts-ignore
-          profileValues[pField] = values[pField];
-        }
-      });
+    const fs = admin.firestore();
+    await fs.runTransaction(async txn => {
+      // @ts-ignore: hack to pass in `transaction`
+      const userStoreInTxn = firebaseStore(User, fs, txn, firebaseConfig);
+      // @ts-ignore: ditto
+      const profileStoreInTxn = firebaseStore(Profile, fs, txn, firebaseConfig);
+      const profileValues = newProfileFor(values);
       userStoreInTxn.update(values);
       profileStoreInTxn.update(profileValues);
     });
     const store = await getDataStore(User);
-    return store.get(values.id);
+    const updatedUser = await store.get(values.id);
+    addDerivedFields(updatedUser!);
+    return updatedUser!;
   },
 );
 
