6a3ada50ee17d196695f14eebe36493590fe3b3b
uidude <22799484+uidude@users.noreply.github.com>
2023-04-28 22:36:47 -0700

    Smoke test (to help with refactoring deps)

---
+++ project/smoke.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+SCRIPTDIR=$(dirname $0)
+BASEDIR=$(cd $SCRIPTDIR/.. && echo $PWD)
+
+# Run yarn install
+sh setup.sh
+pushd $BASEDIR/../../npe-toolkit/shell/latest && yarn install && popd
+
+STATUS=0
+
+# Typecheck all directories
+yarn tsc -p $BASEDIR/client --noEmit
+STATUS=$(($STATUS + $?))
+yarn tsc -p $BASEDIR/admin --noEmit
+STATUS=$(($STATUS + $?))
+yarn tsc -p $BASEDIR/server/functions --noEmit
+STATUS=$(($STATUS + $?))
+
+echo Status: $STATUS
+if [ $STATUS -ne 0 ]; then
+  echo "Failure in typechecking. See logs above to find errors and fix.." && \
+  echo  "To run locally, call \`tools/project/smoke.sh\`"
+  exit 1
+fi
+
