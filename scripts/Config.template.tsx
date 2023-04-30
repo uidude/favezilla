3d523027094d11fe9679b06a915fcf382896a156
uidude <22799484+uidude@users.noreply.github.com>
2023-04-30 10:24:33 -0700

    Publish script

---
+++ scripts/Config.template.tsx
@@ -0,0 +1,50 @@
+import {APP_CONFIG_KEY} from '@toolkit/core/util/AppConfig';
+import {context} from '@toolkit/core/util/AppContext';
+import {FirebaseConfig} from '@toolkit/providers/firebase/Config';
+
+let localConf: Record<string, any> = {};
+try {
+  // @ts-ignore
+  localConf = require('./.localconf.json');
+} catch (_ignored) {}
+
+/**
+ * Fill in the Firebase config from values at
+ * https://console.firebase.google.com/project/YOUR_PROJECT/settings/general/, under "Web apps"
+ */
+export const FIREBASE_CONFIG: FirebaseConfig = localConf['firebase'] ?? {
+  apiKey: 'fill-me-in',
+  authDomain: 'fill-me-in',
+  projectId: 'fill-me-in',
+  storageBucket: 'fill-me-in',
+  messagingSenderId: 'fill-me-in',
+  appId: 'fill-me-in',
+  measurementId: 'fill-me-in',
+  namespace: 'helloworld',
+  emulators: {
+    functions: {
+      useEmulator: false,
+    },
+  },
+};
+
+/**
+ * Fill in the client IDs from
+ * https://console.cloud.google.com/apis/credentials
+ *
+ * You also will need to add redirect URIs in the console, see
+ * https://github.com/facebookincubator/npe-toolkit/blob/main/docs/getting-started/Firebase.md
+ */
+export const GOOGLE_LOGIN_CONFIG = localConf['google-login'] ?? {
+  iosClientId: 'fill-me-in',
+  webClientId: 'fill-me-in',
+  androidClientId: 'fill-me-in',
+};
+
+export const APP_CONFIG = context(APP_CONFIG_KEY, {
+  product: 'helloworld',
+  dataEnv: 'prod',
+  fbAppId: '',
+});
+
+export const CLIENT_FALLBACK_ENABLED = true;
---
+++ scripts/npm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Deploy package to NPM
+# Don't execute this if you are not a maintainer - it won't work
+# and if you're seeing this in your template pacakge it's a bug
+
+# TODOS
+# - Update all files to change helloworld to helloworld
+# - Publish
+# - Git revert everything
+
+SCRIPTDIR=$(dirname $0)
+BASEDIR=$(cd $SCRIPTDIR/../ && echo $PWD)
+
+STATUS=$(git diff --exit-code)
+# Check if files are modified
+if [[ $STATUS -ne 0 ]]; then
+  echo " Can't publish with uncommitted changes."
+  #exit 0
+fi
+
+# Change "helloworld" to "helloword" for Expo app template
+cd $BASEDIR
+git grep -li 'helloworld' | xargs -n 1 sed -i '' 's/[fF]avezilla/helloworld/g' $1
+
+# Replace helloworld back in package.json, as it is getting checked in
+sed -i '' 's/helloworld/helloworld/g' package.json
+
+# Remove Firebase config
+cp scripts/Config.template.tsx common/Config.tsx
+
+yarn publish --access public
+
+# Print message
+echo "NPM repo published!"
+echo 'Commit the new "package.json" with updated version, and revert the remaining files.'
+
