90fc15eb8f2dc5bc5adf26a21c5deb278f6e9ba6
uidude <22799484+uidude@users.noreply.github.com>
2023-05-01 13:00:03 -0700

    Use new admin check and standard error handling in admin panel

--- admin/app/AuthConfig.tsx
+++ admin/app/AuthConfig.tsx
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 import {Account} from '@toolkit/core/api/Auth';
 import {useApi} from '@toolkit/core/api/DataApi';
 import {useBackgroundStatus} from '@toolkit/core/client/Status';
+import {useHasAdminRole} from '@toolkit/core/util/Access';
 import {FirebaseAuthService} from '@toolkit/providers/firebase/client/AuthService';
 import {UnauthorizedError} from '@toolkit/tbd/CommonErrors';
 import {GetUser} from '@app/common/Api';
@@ -10,7 +11,7 @@ import {LoginUserInfo} from '@app/common/AppLogic';
 
 export default function AuthConfig(props: {children?: React.ReactNode}) {
   const getUser = useApi(GetUser);
-  const {setError} = useBackgroundStatus();
+  const hasAdminRole = useHasAdminRole();
 
   /**
    * Use this method to create an instance of your app's user when they log in.
@@ -21,18 +22,14 @@ export default function AuthConfig(props: {children?: React.ReactNode}) {
   ) => {
     throwIfInvalidAccount(firebaseAccount, account.id);
     const loginInfo = loginFields(firebaseAccount);
-    const user = await getUser(loginInfo);
-    // If the user doesn't have roles set or if the user isn't an admin or dev, reject login
-    if (
-      user.roles == null ||
-      !(user.roles.roles.includes('ADMIN') || user.roles.roles.includes('DEV'))
-    ) {
-      const err = UnauthorizedError(
-        "User's roles do not match any allowed roles for this function",
-      );
-      setError(err);
-      throw err;
+    const isAdmin = await hasAdminRole();
+
+    // Note: Server checks will also fail if user is not an admin.
+    // This a just a nice error message vs. trying to render the admin panel
+    if (!isAdmin) {
+      throw UnauthorizedError();
     }
+    const user = await getUser(loginInfo);
 
     return user;
   };
