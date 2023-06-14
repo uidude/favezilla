ab52ae71867bee366a6353fabf9bdd130b85f582
uidude <22799484+uidude@users.noreply.github.com>
2023-06-13 17:50:04 -0700

    Cleanup old comments

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -10,7 +10,7 @@ import {
 } from '@toolkit/providers/firebase/server/Handler';
 
 // Follow the wiki below to enable Firestore security rule enforcement in Functions:
-// https://www.internalfb.com/intern/wiki/NPE/Central_Engineering/NPE_Kit/Guides/Enforcing_Security_Rules_in_Firebase_Functions_or_Server_Code/
+// https://github.com/npe-toolkit/npe-toolkit/blob/main/docs/datastore/server-rules.md
 initFirebaseServer(FIREBASE_CONFIG, APP_CONFIG);
 
 const providers = [FirestoreDatastore, APP_CONFIG];
@@ -22,8 +22,3 @@ initMiddlewares([
 ]);
 
 exports.favezilla = require('./handlers');
-
-// Experimental deletion support - not ready for production
-// Uncomment here and also where installing screens in admin panel to experiment with deletion
-// import * as deletionHandlers from '@toolkit/experimental/deletion/providers/firebase/Deletion';
-// exports.favezilla.deletion = deletionHandlers;
