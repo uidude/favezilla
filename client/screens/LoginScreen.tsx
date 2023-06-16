bbde42588e50ec8660ecb637de44892846cf3d3f
uidude <22799484+uidude@users.noreply.github.com>
2023-06-15 20:22:08 -0700

    Add phone auth (and only phone in prod for now)

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -7,7 +7,7 @@ import {Screen} from '@toolkit/ui/screen/Screen';
 const LOGIN_SCREEN_CONFIG: SimpleLoginScreenConfig = {
   title: 'Favezilla',
   subtitle: 'Share your favorite books!\n(and soon other favorite things)',
-  authTypes: ['google'],
+  authTypes: __DEV__ ? ['google', 'phone'] : ['phone'],
   home: 'Favorites',
   onboarding: 'Onboarding',
   tos: '*Terms of Service coming soon*',
