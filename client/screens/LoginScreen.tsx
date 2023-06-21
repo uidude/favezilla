cf085ee416b2fcad2e07cec4eead384b49ad970f
uidude <22799484+uidude@users.noreply.github.com>
2023-06-21 11:33:27 -0700

    TOS

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -3,14 +3,15 @@ import {
   simpleLoginScreen,
 } from '@toolkit/screens/login/LoginScreen';
 import {Screen} from '@toolkit/ui/screen/Screen';
+import {LOGIN_SCREEN_TOS} from '@app/common/Config';
 
 const LOGIN_SCREEN_CONFIG: SimpleLoginScreenConfig = {
   title: 'Favezilla',
   subtitle: 'Share your favorite books!\n(and soon other favorite things)',
-  authTypes: __DEV__ ? ['google', 'phone', 'apple'] : ['phone'],
+  authTypes: __DEV__ ? ['google', 'phone'] : ['phone'],
   home: 'Favorites',
   onboarding: 'Onboarding',
-  tos: '*Terms of Service coming soon*',
+  tos: LOGIN_SCREEN_TOS,
 };
 
 const LoginScreen: Screen<{}> = simpleLoginScreen(LOGIN_SCREEN_CONFIG);
