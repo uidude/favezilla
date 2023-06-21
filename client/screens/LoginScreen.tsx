19031ff332077aed589cccf7613a7bef84b77edd
uidude <22799484+uidude@users.noreply.github.com>
2023-06-21 16:17:42 -0700

    Official rollout auth methods: apple + google

--- client/screens/LoginScreen.tsx
+++ client/screens/LoginScreen.tsx
@@ -8,7 +8,7 @@ import {LOGIN_SCREEN_TOS} from '@app/common/Config';
 const LOGIN_SCREEN_CONFIG: SimpleLoginScreenConfig = {
   title: 'Favezilla',
   subtitle: 'Share your favorite books!\n(and soon other favorite things)',
-  authTypes: __DEV__ ? ['google', 'phone'] : ['phone'],
+  authTypes: __DEV__ ? ['google', 'phone', 'apple'] : ['google', 'apple'],
   home: 'Favorites',
   onboarding: 'Onboarding',
   tos: LOGIN_SCREEN_TOS,
